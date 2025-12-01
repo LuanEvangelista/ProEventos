752567517f0cc3e0d95f90f525b302864951f284
Luan <luanpereiraevangelista@gmail.com>
2025-12-01 19:34:29 -0300

    Feito validação da parte do Registro.

--- Front/ProEventos-App/src/app/Components/user/registration/registration.ts
+++ Front/ProEventos-App/src/app/Components/user/registration/registration.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink, ReactiveFormsModule],
+
   templateUrl: './registration.html',
   styleUrls: ['./registration.scss'],
 })
-export class Registration {}
+export class Registration {
+  form!: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(public fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  validation(): void {
+    this.form = this.fb.group({
+      primeiroNome: ['', Validators.required],
+      ultimoNome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
+}
